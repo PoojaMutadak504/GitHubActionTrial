feaaef3e6474016b6a13f57352d958d74a886d86
PoojaMutadak504 <48515218+PoojaMutadak504@users.noreply.github.com>
2023-03-13 16:22:43 +0530

    txt file

---
+++ TestMonsterFiles/integrations.java
@@ -0,0 +1,40 @@
+package com.testmonster.tests;
+
+import java.net.MalformedURLException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.testmonster.commons.Library;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class integrations {
+	public AppiumDriver<MobileElement> driver;
+	public Library lib;
+
+	@BeforeClass
+	public void setup() throws MalformedURLException {
+		lib = new Library("Test Started");
+		driver = lib.launchDriver();
+
+	}
+
+	@Test
+	public void integrationsTest() throws Exception {
+                  lib.findMobileElement("id:nextButton");
+lib.clickMobileElement("id:nextButton");
+lib.clickMobileElement("id:btnShowLogin");
+lib.enterMobileText("id:edtName","pooja");
+lib.enterMobileText("id:edtPass","pooja");
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		lib.quitBrowser();
+	}
+
+}
