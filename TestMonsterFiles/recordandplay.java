166c7e747ae90e15690b18133657945620c795e9
PoojaMutadak504 <48515218+PoojaMutadak504@users.noreply.github.com>
2023-03-17 17:05:57 +0530

    txt file

---
+++ TestMonsterFiles/recordandplay.java
@@ -0,0 +1,43 @@
+package com.testmonster.tests;
+
+import java.net.MalformedURLException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.testmonster.commons.Library;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class recordandplay {
+	public AppiumDriver<MobileElement> driver;
+	public Library lib;
+
+	@BeforeClass
+	public void setup() throws MalformedURLException {
+		lib = new Library("Test Started");
+		driver = lib.launchDriver();
+
+	}
+
+	@Test
+	public void recordandplayTest() throws Exception {
+                  lib.clickMobileElement("id:nextButton");
+lib.sleep(200);
+lib.clickMobileElement("id:btnShowLogin");
+lib.sleep(200);
+lib.enterMobileText("id:edtName","pooja");
+lib.sleep(200);
+lib.enterMobileText("id:edtPass","pooja@123");
+lib.clickMobileElement("id:btnSignIn");
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		lib.quitBrowser();
+	}
+
+}
